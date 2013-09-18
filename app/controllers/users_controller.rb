09328c30fab88ee98bad592f7ddbe9cefa587c13
Carlos Ledezma <cgledezma1101@gmail.com>
2013-09-18 17:07:51 -0430

    Added validation on the submission of the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(params[:user])
+#    @user = User.new(user_params) # This is the preferred method in Rails 4.0
+    if @user.save
+      # Handle a successful save
+    else
+      render 'new'
+    end
+  end
+
   def new
     @user = User.new
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
