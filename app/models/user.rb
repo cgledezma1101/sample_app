4849fa3cc815dea4f7c67fdccd680b4df0315dd8
Carlos Ledezma <cgledezma1101@gmail.com>
2013-09-18 10:35:43 -0430

    Added validations over user's names and emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  # Callbacks
+  before_save { self.email = email.downcase }
+
+  # Validations of the attributes
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX  = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false}
 end
