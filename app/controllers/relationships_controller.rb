8e6ec232336b19ea45da307945263b04fde094c4
Carlos Ledezma <cgledezma1101@gmail.com>
2013-09-20 17:54:50 -0430

    Alternative implementation to Ajax response

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,21 +5,26 @@ class RelationshipsController < ApplicationController
 # Since it's not implemented on Rails 3.2, we use this:
   before_filter :signed_in_user
 
+  respond_to :html, :js
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    respond_with @user
+# A way of doing it without respond_to up there
+#    respond_to do |format|
+#      format.html { redirect_to @user }
+#      format.js
+#    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    respond_with @user
+#    respond_to do |format|
+#      format.html { redirect_to @user }
+#      format.js
+#    end
   end
 end
